e0ad408a17346abc868cabf498b558eb17c45621
aryeh <aryehzinn@gmail.com>
2019-03-01 18:51:15 +1100

    Refactoring P2
    Refactored more code and added some coments. Need to finish refactoring, as a few major issues were identified within the code.

--- UntitledPlatformerProject/Assets/Scripts/Player.cs
+++ UntitledPlatformerProject/Assets/Scripts/Player.cs
@@ -7,14 +7,6 @@ public class Player : MonoBehaviour {
 
     Controller2D controller;
 
-    delegate void HorizontalMovementHandler();
-
-    [SerializeField]
-    event HorizontalMovementHandler MovedHorizontal;
-
-    
-
-
 	void Start () {
 
         controller = GetComponent<Controller2D>();
@@ -22,14 +14,16 @@ public class Player : MonoBehaviour {
 
 	void Update () {
 
+        controller.ApplyGravity();
+
         Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
 
-        if (Input.GetKeyDown(KeyCode.Space) && controller.collisionInformation.isBelow) {
+        controller.MoveHorizontal(input);
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
 
             controller.Jump();
 
         }
-
-        controller.MoveHorizontal(input);
     }
 }
