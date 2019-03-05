494f03c9ce1198dfd1d7e3242114005d5f77b42d
aryeh <aryehzinn@gmail.com>
2019-03-05 17:25:12 +1100

    Code changes and Platform Jumping
    Added functionality to allow players to jump through specific platforms. Still a bit clunky, and platform code is stored in the playercontroller. Will need to migrate the implementation into another class to keep things more modular.

---
+++ UntitledPlatformerProject/Assets/Scripts/PlatformerScripts/Platform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : RayCastUser {
+
+    [SerializeField]
+    BoxCollider2D collider;
+	
+	public override void Start () {
+
+        base.Start();
+	}
+	
+	void Update () {
+		
+	}
+
+    void DetectPlayersBelow() {
+
+        
+
+    }
+}
