d5448034c1e56f656145384b93fedece3082b31d
aryeh <aryehzinn@gmail.com>
2019-03-11 12:27:51 +1100

    Mouse Detection Raycast
    The level editor now detects input fromt he mouse and can select tiles in the selection bar. Next steps are to ensure that the tile can be fixed to the mouse and snapped to the overworld grid.

--- UntitledPlatformerProject/Assets/Scripts/LevelEditor/ToolSet.cs
+++ UntitledPlatformerProject/Assets/Scripts/LevelEditor/ToolSet.cs
@@ -1,22 +1,88 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ToolSet : MonoBehaviour {
 
-    Tile currentTile;
+    Collider2D currentTileCollider;
+
+    [SerializeField]
+    [ReadOnly] Tile currentTile;
+
+    [SerializeField]
+    TextMeshPro text;
+
+    [SerializeField]
+    LayerMask layerMask;
+
+    [SerializeField]
+    int rayLength;
+
+    bool clicked;
+
+    Vector2 mousePosition;
 
     private void Update() {
-        
 
+        CheckMouseinput();
     }
 
-    Tile SelectTile() {
+    void MouseHover() {
 
-        Tile selectedTile = null;
+        RaycastHit2D hit;
+
+        hit = Physics2D.Raycast(mousePosition, Vector2.up * rayLength, layerMask);
+
+        if (hit) {
+
+            UpdateText(hit.transform.name);
+
+            if (clicked) {
+
+                if (currentTileCollider != hit.collider) {
+
+                    currentTileCollider = hit.collider;
+                    currentTile = hit.transform.GetComponent<Tile>();
+                }
+            }
+
+            clicked = false;
+
+        } else {
+            UpdateText("None");
+        }
+    }
+
+    void CheckMouseinput() {
+
+        Vector2 currentMousePosition = GetMousePosition();
+
+        if (currentMousePosition != mousePosition) {
+
+            mousePosition = currentMousePosition;
+
+            if (Input.GetKeyDown(KeyCode.Mouse0)) {
+
+                clicked = true;
+            }
 
 
+            MouseHover();
+        }
+    }
 
-        return selectedTile;
+    void UpdateText(string name) {
+
+        text.text = "Tile: " + name;
+    }
+
+    Vector2 GetMousePosition() {
+
+        Vector2 mousePosition = Vector2.zero;
+
+        mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+
+        return mousePosition;
     }
 }
