f7c1be9005166e82e46d7363ed35387c392a2c8a
aryeh <aryehzinn@gmail.com>
2019-03-15 17:36:37 +1100

    Added capabilities for tiles to be placed appropriately in the background, foreground, and midground. Also added an eraser tool (right click on a tile).

--- UntitledPlatformerProject/Assets/Scripts/LevelEditor/PlaceHolderTile.cs
+++ UntitledPlatformerProject/Assets/Scripts/LevelEditor/PlaceHolderTile.cs
@@ -6,7 +6,12 @@ public class PlaceHolderTile : MonoBehaviour
 {
     public SpriteRenderer Renderer { get { return renderer; } }
 
+    public int SettingsId { get { return settingsId; } set { settingsId = value; } } 
+
     SpriteRenderer renderer;
+
+    [SerializeField, ReadOnly]
+    int settingsId;
 
     private void Awake() {
         renderer = GetComponent<SpriteRenderer>();
