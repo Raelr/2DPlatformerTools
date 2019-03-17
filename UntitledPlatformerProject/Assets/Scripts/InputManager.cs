a4f41397ffd93c3a0180ecf77068ff6f14d384f8
aryeh <aryehzinn@gmail.com>
2019-03-18 10:35:51 +1100

    Added an input manager class to handle all player inputs. Also added functionality to constrain tile placement to function within the screen bounds only.

---
+++ UntitledPlatformerProject/Assets/Scripts/InputManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InputManager : MonoBehaviour
+{
+    public delegate void ArrowKeysUsed();
+
+    public event ArrowKeysUsed keyboardInput;
+
+    public delegate void MouseMoved();
+
+    public event MouseMoved mouseMoved;
+
+    public UnityEvent UpdateEvent;
+
+    private void Awake() {
+
+        if (UpdateEvent == null) {
+            UpdateEvent = new UnityEvent();
+        }
+    }
+
+    void Update()
+    {
+        if (UpdateEvent != null) {
+
+            UpdateEvent.Invoke();
+        }
+    }
+}
